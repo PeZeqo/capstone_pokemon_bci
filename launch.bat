d787adeadc063586ed9d1a38418dd203333b72a2
PeZeqo <peterzeqo@gmail.com>
2020-10-07 13:48:54 -0400

    fixing launch.bat, removing env from github

--- launch.bat
+++ launch.bat
@@ -1,1 +1,6 @@
-call capstone-env\Scripts\activate && python -m testing_window
+START /WAIT python -m pip install --user vitualenv
+START /WAIT python -m venv capstone-env
+CALL .\capstone-env\Scripts\activate
+START /WAIT python -m pip install -r requirements.txt
+START /WAIT python -m gaming_window
+deactivate
