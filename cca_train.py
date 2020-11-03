48f2d134cd60777b24fa26e025d397f740681d4a
Matthew Heim <matt@heimfamily.org>
2020-11-03 11:29:11 -0500

    update frequencies, still bad results

--- cca_train.py
+++ cca_train.py
@@ -1,4 +1,5 @@
-# import pandas as pd
+from pynput.keyboard import Key, Controller
+import pandas as pd
 import numpy as np
 from sklearn.cross_decomposition import CCA
 
@@ -7,50 +8,86 @@ class cca_handler():
 	def __init__(self):
 		self.num_targets = 8
 		self.sampling_rate = 128.0
-		self.frequencies = [30.0, 20.0, 15.0, 12.0, 8.57, 7.5, 6.67]
-		self.ref_signals_1s = self.getReferenceSignals1s() 
+		# self.frequencies = [30.0, 20.0, 15.0, 12.0, 10.0,   8.57, 7.5, 6.67]
+		# frequencies calculated by frames/len(array) as seen in flicker_patterns.txt
+		self.frequencies = [32.0, 21.33, 14.22, 42.67, 16.0, 64.0, 25.6, 18.29]
 		self.prediction = None # target 0 to num_targets-1
-		self.input_data_1s = None
+		self.keyboard = Controller()
+
+		self.ref_signals = []
+		self.getReferenceSignals1s()
+
+		self.cca = CCA(n_components=1)
+
 
 	def getReferenceSignals1s(self):
+		# assume 128 samples 
 		for freq in self.frequencies:
 			self.ref_signals.append(self.generateReferenceSignal(128, freq))
+		self.ref_signals = np.asarray(self.ref_signals)
 
 
 	def generateReferenceSignal(self, samples, target_freq):
+		# get references for one frequency
 		reference_signals = []
 		t = np.arange(0, (samples/self.sampling_rate), step=1.0/self.sampling_rate)
 		reference_signals.append(np.sin(np.pi*2*target_freq*t))
 		reference_signals.append(np.cos(np.pi*2*target_freq*t))
 		reference_signals.append(np.sin(np.pi*4*target_freq*t))
 		reference_signals.append(np.cos(np.pi*4*target_freq*t))
-		reference_signals = np.array(reference_signals)
+		reference_signals = np.asarray(reference_signals)
 
 		return reference_signals
 
 
-	def findCorr(input_data, target_signal):
-		cca = CCA(n_components=1)
+	def findCorr(self, input_data, target_signal):
 		result = np.zeros((target_signal.shape)[0])
 		for i in range(0, (target_signal.shape)[0]):
-			cca.fit(input_data, np.squeeze(target_signal[i,:,:]).T)
-			a, b = cca.transform(input_data, np.squeeze(target_signal[i,:,:]).T)
+			self.cca.fit(input_data, np.squeeze(target_signal[i,:,:]).T)
+			a, b = self.cca.transform(input_data, np.squeeze(target_signal[i,:,:]).T)
 			corr = np.corrcoef(a[:,0], b[:,0])[0, 1]
+			print('correlation target {} = {}'.format(i, corr))
 			result[i] = corr
 		return result
 
+	def filter(self, data):
+		# data shape is 128 x 4
+		return data
+
 	def predict(self, data):
 		# pass in the 1s sample
-		self.input_data_1s = data
-		corrs = [None] * num_targets
-		for i in range(num_targets):
-			corrs[i] = findCorr(self.input_data_1s, ref_signals_1s[i])
+		print("Make Prediction")
+		data = self.filter(data)
+		corrs = self.findCorr(data, self.ref_signals)
 		self.prediction = np.argmax(corrs)
-			
+		print("Predicted Command: {}".format(self.prediction))
+		self.command_to_keyboard_action(self.prediction)
 
-	def getPrediction(self):
-		self.predict()
-		return self.prediction
+
+	def press_release(self, key):
+		self.keyboard.press(key)
+		self.keyboard.release(key)
+
+	def command_to_keyboard_action(self, command):
+		# command 0 is no-action
+		if command == 0:
+			return
+		elif command == 1:
+			self.press_release('x')
+		elif command == 2:
+			self.press_release(Key.up)
+		elif command == 3:
+			self.press_release('z')
+		elif command == 4:
+			self.press_release(Key.left)
+		elif command == 5:
+			self.press_release(Key.right)
+		elif command == 6:
+			self.press_release(Key.enter)
+		elif command == 7:
+			self.press_release(Key.down)
+		elif command == 8:
+			self.press_release(Key.shift_r)
 
 
 
--- live_predicting_window.py
+++ live_predicting_window.py
@@ -1,279 +1,304 @@
-# live predicting window, but import csv as user input
-# I want the arrows to point to where the correct input is
-# perhaps a square around the chosen input 
-
-# no cortex needeed? and im not recording
-# instead of export_recording, import_recording
-
+# live predicting window, and import csv as user input
+# arrow should point to the correct input
+# TODO: perhaps a square around the chosen input 
+# used some parts from data_collection_window, command_handler, gaming_window
 
 import os
 import arcade
 from project_constants import FREQUENCY, PATTERN_LENGTH, PADDING, BASE_HEIGHT, BASE_WIDTH, ARROW_SIZE, ARROW_SCALE, \
-    CHECKERBOARD_SIZE, RECORDINGS_PER_ICON, SECONDS_PER_RECORDING, PAUSE_TO_RECORD_RATIO, COMMAND_SEND_FREQUENCY
-from cortex.cortex import Cortex
+	CHECKERBOARD_SIZE, RECORDINGS_PER_ICON, SECONDS_PER_RECORDING, PAUSE_TO_RECORD_RATIO, COMMAND_SEND_FREQUENCY
 import pandas as pd
 import numpy as np
+from pyboy import PyBoy
+from pyboy.utils import WindowEvent
+import sdl2
+from command_handler import command_handler
+from cca_train import cca_handler
+import ctypes
+import time
+import re
 
 
 class live_predicting_window(arcade.Window):
 
 	# screen and image size vars
-    screen_width = None
-    screen_height = None
-    checkerboard_size = CHECKERBOARD_SIZE
-    arrow_size = ARROW_SIZE * ARROW_SCALE
-    padding = PADDING
+	screen_width = None
+	screen_height = None
+	checkerboard_size = CHECKERBOARD_SIZE
+	arrow_size = ARROW_SIZE * ARROW_SCALE
+	padding = PADDING
 
-    # textures and frequencies vars
-    texture_list = []
-    arrow_textures = []
-    flicker_frequency = []
-    last_state = []
-    checkerboard_pos_list = []
-    draw_scale = None
-    tick = 0
-    selected_arrow = 0
+	# textures and frequencies vars
+	texture_list = []
+	arrow_textures = []
+	flicker_frequency = []
+	last_state = []
+	checkerboard_pos_list = []
+	draw_scale = None
+	tick = 0
+	selected_arrow = 0
 
-    # recording control vars
-    currently_recording = False
-
-
-    # data collection vars
-    generator = None
-    recording_data = []
-
-    # previously recorded data
-    recorded_data = []
-
-    def __init__(self, width, height, title):
-        # set total screen size since we also need space to draw checkerboards
-        self.screen_width = self.arrow_size + CHECKERBOARD_SIZE * 2 + PADDING
-        self.screen_height = self.arrow_size + CHECKERBOARD_SIZE * 2 + PADDING
-
-        self.draw_scale = self.screen_height / BASE_HEIGHT
-
-        # let Arcade Window run it's setup
-        super().__init__(self.screen_width, self.screen_height, title, resizable=True)
-
-        # set frame rate as (1 / desired_frame_rate)
-        self.set_update_rate(1/FREQUENCY)
-
-    def setup(self):
-        self.setup_checkerboards()
-        self.load_arrows()
-        # self.setup_cortex()
-
-    # def setup_cortex(self):
-    #     self.cortex = Cortex(None)
-    #     self.cortex.do_prepare_steps()
-    #     self.generator = self.cortex.sub_request(['eeg'])
-    #     self.recording_data = pd.DataFrame(columns=self.data_columns)
-
-    def setup_checkerboards(self):
-        # load textures
-        self.load_checkerboards()
-
-        self.resize_drawing_vars()
-
-        self.set_checkerboard_positions()
-
-        # set all checkerboards to normal state (as opposed to inverted checkerboard)
-        self.last_state = [0] * len(self.checkerboard_pos_list)
-
-        # set flicker frequencies for each quadrant
-        with open('flicker_patterns.txt') as f:
-            for sequence in f:
-                self.flicker_frequency.append(eval(sequence))
+	# recording control vars
+	currently_recording = False
 
 
-    def set_checkerboard_positions(self):
-        # set some useful vars
-        half_width = self.screen_width / 2
-        half_height = self.screen_height / 2
-        half_checkerboard = self.checkerboard_size / 2
-        width_offset = (self.arrow_size / 2) + half_checkerboard + self.padding
-        height_offset = (self.arrow_size / 2) + half_checkerboard + self.padding
+	# data collection vars
+	generator = None
+	# recording data is imported from csv
+	recording_data = []
 
-        # empty checkerboard pos list
-        self.checkerboard_pos_list = []
+	# length of recording data
+	record_length = None
 
-        # prep x,y pairs for where to print checkerboards
-        for x in [half_width - width_offset, half_width, half_width + width_offset]:
-            for y in [half_height - height_offset, half_height, half_height + height_offset]:
-                # skip drawing in the middle of board
-                if x == half_width and y == half_height:
-                    continue
-                self.checkerboard_pos_list.append([x, y])
+	# row number index
+	row_index = 0
+	channels = ['P7', 'O1', 'O2', 'P8']
 
-    def load_checkerboards(self):
-        # Set up dir paths
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        image_dir = os.path.join(dir_path, 'images')
-        checkerboard_dir = os.path.join(image_dir, 'checkerboards')
-        icon_list = os.listdir(checkerboard_dir)
+	# ML vars
+	command_handler = None
 
-        # empty texture list, load new textures
-        self.texture_list = []
-        for icon in icon_list:
-            if "png" not in icon.lower():
-                continue
-            self.texture_list.append(arcade.load_texture(os.path.join(checkerboard_dir, icon)))
+	def __init__(self, width, height, title, recording_name='recordings/first_target_0.csv'):
+		# set total screen size since we also need space to draw checkerboards
+		self.screen_width = self.arrow_size + CHECKERBOARD_SIZE * 2 + PADDING
+		self.screen_height = self.arrow_size + CHECKERBOARD_SIZE * 2 + PADDING
 
-    def load_arrows(self):
-        # Set up dir paths
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        image_dir = os.path.join(dir_path, 'images')
-        arrow_dir = os.path.join(image_dir, 'arrows')
-        icon_list = os.listdir(arrow_dir)
+		self.draw_scale = self.screen_height / BASE_HEIGHT
 
-        for icon in icon_list:
-            if "png" not in icon.lower():
-                continue
-            self.arrow_textures.append(arcade.load_texture(os.path.join(arrow_dir, icon)))
+		# let Arcade Window run it's setup
+		super().__init__(self.screen_width, self.screen_height, title, resizable=True)
 
-    def on_update(self, delta_time):
-        # self.on_draw()
-        self.exhaust()
-        self.tick += 1
-        if not self.wait_on_user:
-            self.handle_recording()
+		# set frame rate as (1 / desired_frame_rate)
+		self.set_update_rate(1/FREQUENCY)
 
-        # if self.tick % COMMAND_SEND_FREQUENCY == 0:
-        #     self.create_command_task()
+		# set recording name
+		self.recording_name = recording_name
+		# set arrow target - assume target num is in the name
+		self.target = int(re.findall(r'\d+|$', self.recording_name)[0])
+		print('Choose target {}'.format(self.target))
 
-    def create_command_task(self):
-        pass
+		# import
+		self.import_recording()
 
-    # def beep(self):
-    #     winsound.Beep(self.beep_frequency, self.beep_duration)
+	def print_stage(self, stage):
+		print('-'*20)
+		print('{} BEGINNING'.format(stage))
+		print('-'*20)
 
-    def exhaust(self):
-        next(self.generator)
+	def setup(self):
+		self.print_stage("VISUALS SETUP")
+		self.setup_checkerboards()
+		self.load_arrows()
+		self.print_stage("PYBOY SETUP")
+		self.setup_pyboy()
+		self.print_stage("COMMAND HANDLER SETUP")
+		self.setup_command_handler()
 
-    # def add_recording(self):
-    #     to_append = np.asarray(list(next(self.generator).queue))
-    #     a_series = pd.DataFrame(to_append, columns=self.recording_data.columns)
-    #     self.recording_data = self.recording_data.append(a_series, ignore_index=True)
+	def setup_command_handler(self):
+		self.command_handler = cca_handler()
 
-    # def export_recording(self):
-    #     self.recording_data.to_csv('recordings\\checkerboard_recording_sultan_{}.csv'.format(self.selected_arrow))
-    #     self.recording_data.drop(self.recording_data.index, inplace=True)
+	def setup_checkerboards(self):
+		# load textures
+		self.load_checkerboards()
 
-    def import_recording(self):
+		self.resize_drawing_vars()
+
+		self.set_checkerboard_positions()
+
+		# set all checkerboards to normal state (as opposed to inverted checkerboard)
+		self.last_state = [0] * len(self.checkerboard_pos_list)
+
+		# set flicker frequencies for each quadrant
+		with open('flicker_patterns.txt') as f:
+			for sequence in f:
+				self.flicker_frequency.append(eval(sequence))
 
 
-    def handle_recording(self):
-        # if not on pause and the allotted pause time has passed, start recording again
-        # if not self.currently_recording and self.tick == self.pause_ticks:
-        #     self.currently_recording = True
-        #     self.tick = 0
-        #     self.beep()
+	def set_checkerboard_positions(self):
+		# set some useful vars
+		half_width = self.screen_width / 2
+		half_height = self.screen_height / 2
+		half_checkerboard = self.checkerboard_size / 2
+		width_offset = (self.arrow_size / 2) + half_checkerboard + self.padding
+		height_offset = (self.arrow_size / 2) + half_checkerboard + self.padding
 
-        # # if recording and the allotted recording time has passed, start pause again
-        # elif self.currently_recording and self.tick == self.record_ticks:
-        #     self.add_recording()
-        #     self.currently_recording = False
-        #     self.recordings_done += 1
-        #     self.tick = 0
-        #     self.beep()
+		# empty checkerboard pos list
+		self.checkerboard_pos_list = []
 
-        # finished all recordings for this icon
-        # if self.recordings_done == RECORDINGS_PER_ICON:
-        #     self.recordings_done = 0
-        #     self.export_recording()
-        #     self.wait_on_user = True
-        #     self.selected_arrow += 1
+		# prep x,y pairs for where to print checkerboards
+		for x in [half_width - width_offset, half_width, half_width + width_offset]:
+			for y in [half_height - height_offset, half_height, half_height + height_offset]:
+				# skip drawing in the middle of board
+				if x == half_width and y == half_height:
+					continue
+				self.checkerboard_pos_list.append([x, y])
 
-        #     # went through all arrows, means we recorded all checkerboards
-        #     if self.selected_arrow == len(self.arrow_textures):
-        #         self.exit_window()
+	def load_checkerboards(self):
+		# Set up dir paths
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		image_dir = os.path.join(dir_path, 'images')
+		checkerboard_dir = os.path.join(image_dir, 'checkerboards')
+		icon_list = os.listdir(checkerboard_dir)
 
-        # finished running through csv file
-        if:
-        	self.exit_window()
+		# empty texture list, load new textures
+		self.texture_list = []
+		for icon in icon_list:
+			if "png" not in icon.lower():
+				continue
+			self.texture_list.append(arcade.load_texture(os.path.join(checkerboard_dir, icon)))
 
-    def resize_drawing_vars(self):
-        if self.screen_height <= self.screen_width:
-            self.draw_scale = self.screen_height / BASE_HEIGHT
-        else:
-            self.draw_scale = self.screen_width / BASE_WIDTH
-        self.checkerboard_size = CHECKERBOARD_SIZE * self.draw_scale
-        self.arrow_size = ARROW_SIZE * ARROW_SCALE * self.draw_scale
-        self.arrow_size = ARROW_SIZE * ARROW_SCALE * self.draw_scale
-        self.padding = PADDING * self.draw_scale
+	def setup_pyboy(self):
+		# load rom through PyBoy
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		rom_dir = os.path.join(dir_path, 'roms')
+		self.pyboy = PyBoy(os.path.join(rom_dir, 'Pokemon.gb'))
 
-    def on_resize(self, width, height):
-        """ This method is automatically called when the window is resized. """
+		# set up PyBoy screen support
+		self.bot_sup = self.pyboy.botsupport_manager()
+		self.scrn = self.bot_sup.screen()
 
-        # Call the parent. Failing to do this will mess up the coordinates, and default to 0,0 at the center and the
-        # edges being -1 to 1.
-        super().on_resize(width, height)
+		# minimize PyBoy window
+		pyboy_handle = ctypes.windll.user32.FindWindowW(None, "PyBoy")
+		ctypes.windll.user32.ShowWindow(pyboy_handle, 6)
 
-        self.screen_width = width
-        self.screen_height = height
+	def load_arrows(self):
+		# Set up dir paths
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		image_dir = os.path.join(dir_path, 'images')
+		arrow_dir = os.path.join(image_dir, 'arrows')
+		icon_list = os.listdir(arrow_dir)
 
-        self.resize_drawing_vars()
+		for icon in icon_list:
+			if "png" not in icon.lower():
+				continue
+			self.arrow_textures.append(arcade.load_texture(os.path.join(arrow_dir, icon)))
 
-        self.set_checkerboard_positions()
+	def on_update(self, delta_time):
+		# check if done
+		if self.row_index >= self.record_length:
+			self.exit_window()
+		self.pyboy.tick()
+		# self.on_draw()
+		# self.exhaust()
+		self.tick += 1
+		self.tick %= FREQUENCY
+		if self.tick % COMMAND_SEND_FREQUENCY == 0:
+			"Starting guess"
+			start = time.time()
+			self.command_handler.predict(self.get_eeg_data())
+			print("Guess done in: {}s".format(time.time() - start))
+			self.row_index += 128
 
-    def on_draw(self):
-        # Start the render process
-        arcade.start_render()
+	# def exhaust(self):
+	# 	next(self.generator)
 
-        # Draw game
-        self.draw_arrow()
-        self.draw_timer()
+	def get_eeg_data(self):
+		# return np.asarray(list(next(self.generator).queue))
+		# get 128 rows of recording_data
+		return np.asarray(self.recording_data[self.channels][self.row_index:self.row_index + 128])
 
-        # Draw checkerboards
-        self.draw_checkerboards()
 
-        # Finish the render
-        arcade.finish_render()
+	def import_recording(self):
+		if 'recordings/' not in self.recording_name:
+			self.recording_name = 'recordings/' + self.recording_name
+		self.recording_data = pd.read_csv(self.recording_name)
+		self.record_length = len(self.recording_data.index)
 
-    def draw_arrow(self):
-        arcade.draw_scaled_texture_rectangle(self.width / 2, self.height / 2, self.arrow_textures[self.selected_arrow],
-                                             ARROW_SCALE * self.draw_scale)
+	def resize_drawing_vars(self):
+		if self.screen_height <= self.screen_width:
+			self.draw_scale = self.screen_height / BASE_HEIGHT
+		else:
+			self.draw_scale = self.screen_width / BASE_WIDTH
+		self.checkerboard_size = CHECKERBOARD_SIZE * self.draw_scale
+		self.arrow_size = ARROW_SIZE * ARROW_SCALE * self.draw_scale
+		self.arrow_size = ARROW_SIZE * ARROW_SCALE * self.draw_scale
+		self.padding = PADDING * self.draw_scale
 
-    def draw_timer(self):
-        arcade.draw_text("Timer: " + str(round(self.tick / FREQUENCY, 2)), (self.width / 2) - (PADDING * 1.2) * self.draw_scale,
-                         (self.height + self.arrow_size) / 2 - (PADDING * self.draw_scale), arcade.color.WHITE,
-                         40 * self.draw_scale)
+	def on_resize(self, width, height):
+		""" This method is automatically called when the window is resized. """
 
-        arcade.draw_text("Recordings Left: " + str(RECORDINGS_PER_ICON - self.recordings_done), (self.width / 2) - (PADDING * 2) * self.draw_scale,
-                         (self.height - self.arrow_size) / 2 + (PADDING * self.draw_scale), arcade.color.WHITE,
-                         40 * self.draw_scale)
+		# Call the parent. Failing to do this will mess up the coordinates, and default to 0,0 at the center and the
+		# edges being -1 to 1.
+		super().on_resize(width, height)
 
-    def draw_checkerboards(self):
-        # Load and draw all checkerboards
-        for ind, last_state in enumerate(self.last_state):
-            freq = self.flicker_frequency[ind]
-            pos = self.checkerboard_pos_list[ind]
-            texture = self.texture_list[last_state]
+		self.screen_width = width
+		self.screen_height = height
 
-            # if this is a switch state, change checkerboard state:
-            if freq[self.tick % len(freq)]:
-                self.last_state[ind] = not last_state
+		self.resize_drawing_vars()
 
-            arcade.draw_scaled_texture_rectangle(pos[0], pos[1], texture, self.draw_scale, 0)
+		self.set_checkerboard_positions()
 
-    def on_key_press(self, key, key_modifiers):
-        # switch statement to control data collection
-        if (key == arcade.key.SPACE):
-            self.wait_on_user = False
-            self.currently_recording = False
-            self.tick = 0
+	def on_draw(self):
+		# Start the render process
+		arcade.start_render()
 
-    def exit_window(self):
-        exit()
+		# Draw game
+		self.draw_arrow()
+		self.draw_timer()
 
-    # def main():
-    #     window = data_collection_window(0, 0, "Data Collection Window")
-    #     window.setup()
-    #     arcade.run()
+		# Draw checkerboards
+		self.draw_checkerboards()
+
+		# Finish the render
+		arcade.finish_render()
+
+	def draw_arrow(self):
+		arcade.draw_scaled_texture_rectangle(self.width / 2, self.height / 2, self.arrow_textures[self.target],
+											 ARROW_SCALE * self.draw_scale)
+
+	def draw_timer(self):
+		arcade.draw_text("Timer: " + str(round(self.tick / FREQUENCY, 2)), (self.width / 2) - (PADDING * 1.2) * self.draw_scale,
+						 (self.height) / 2 - (PADDING * self.draw_scale), arcade.color.WHITE,
+						 40 * self.draw_scale)
+
+	def draw_checkerboards(self):
+		# Load and draw all checkerboards
+		for ind, last_state in enumerate(self.last_state):
+			freq = self.flicker_frequency[ind]
+			pos = self.checkerboard_pos_list[ind]
+			texture = self.texture_list[last_state]
+
+			# if this is a switch state, change checkerboard state:
+			if freq[self.tick % len(freq)]:
+				self.last_state[ind] = not last_state
+
+			arcade.draw_scaled_texture_rectangle(pos[0], pos[1], texture, self.draw_scale, 0)
+
+	def on_key_press(self, key, key_modifiers):
+		actions = []
+		print("KEY PRESSED: {}".format(key))
+
+		# switch statement to create WindowEvents (button commands for PyBoy)
+		if (key == arcade.key.UP):
+			actions = [WindowEvent.PRESS_ARROW_UP, WindowEvent.RELEASE_ARROW_UP]
+		elif (key == arcade.key.DOWN):
+			actions = [WindowEvent.PRESS_ARROW_DOWN, WindowEvent.RELEASE_ARROW_DOWN]
+		elif (key == arcade.key.LEFT):
+			actions = [WindowEvent.PRESS_ARROW_LEFT, WindowEvent.RELEASE_ARROW_LEFT]
+		elif (key == arcade.key.RIGHT):
+			actions = [WindowEvent.PRESS_ARROW_RIGHT, WindowEvent.RELEASE_ARROW_RIGHT]
+		elif (key == arcade.key.Z):
+			actions = [WindowEvent.PRESS_BUTTON_B, WindowEvent.RELEASE_BUTTON_B]
+		elif (key == arcade.key.X):
+			actions = [WindowEvent.PRESS_BUTTON_A, WindowEvent.RELEASE_BUTTON_A]
+		elif (key == arcade.key.ENTER):
+			actions = [WindowEvent.PRESS_BUTTON_SELECT, WindowEvent.RELEASE_BUTTON_SELECT]
+		elif (key == arcade.key.RSHIFT):
+			actions = [WindowEvent.PRESS_BUTTON_START, WindowEvent.RELEASE_BUTTON_START]
+
+		# if actions list isn't empty, send commands to pyboy and tick the pyboy window to process each command
+		if actions:
+			for action in actions:
+				self.pyboy.send_input(WindowEvent(action))
+				self.pyboy.tick()
+
+	def exit_window(self):
+		exit()
+
+	def main():
+		window = live_predicting_window(0, 0, "Live Predicting Window")
+		window.setup()
+		arcade.run()
 
 
 if __name__ == "__main__":
-    # data_collection_window.main()
-    window = live_predicting_window(0, 0, "Prediction Window")
-    window.setup()
-    arcade.run()
+	live_predicting_window.main()
